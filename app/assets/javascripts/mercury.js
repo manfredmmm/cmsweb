d9c66622b08f30979836bc77f1e09ef8f37d659a
manfred <manfred@imesmes.com>
2011-11-21 17:42:14 +0100

    Mercury running!

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -381,6 +381,16 @@ window.Mercury = {
 // The mercury:loaded event is provided in case you want to override default Mercury Editor behavior.  It will fire
 // directly after the Mercury scripts have loaded, but before anything has been initialized.  It's a good place to add
 // or change functionality.
-jQuery(window).bind('mercury:loaded', function() {
+//jQuery(window).bind('mercury:loaded', function() {
   //Mercury.PageEditor.prototype.iframeSrc = function(url) { return '/testing'; }
+//});
+
+$(window).bind('mercury:ready', function() {
+  var link = $('#mercury_iframe').contents().find('#edit_link');
+  Mercury.saveURL = link.data('save-url');
+  link.hide();
+});
+
+$(window).bind('mercury:saved', function() {
+  window.location = window.location.href.replace(/\/editor\//i, '/');
 });
--- app/controllers/web_spaces_controller.rb
+++ app/controllers/web_spaces_controller.rb
@@ -1,4 +1,12 @@
 class WebSpacesController < AuthorizedController
   inherit_resources
   belongs_to :teacher
+
+  def mercury_update
+    page = WebSpace.find(params[:id])
+    page.name = params[:content][:page_name][:value]
+    page.content = params[:content][:page_content][:value]
+    page.save!
+    render text: ""
+  end
 end
