8f1d56e8fe16bd57aeb0cd83963da2bd1eec70a7
manfred <manfred@imesmes.com>
2011-11-23 16:59:55 +0100

    Added style for flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require_self
-//= require_tree .
+//= require_directory .
+
+$(function() {
+  setTimeout(function(){ $('.flash').hide("fade", {}, 1000) }, 2000);
+});
