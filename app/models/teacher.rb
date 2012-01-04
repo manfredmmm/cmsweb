d49b73387a632952d32ff94377c6489d09b47213
manfred <manfred@imesmes.com>
2012-01-04 16:43:47 +0100

    Admin teacher/department CRUD views

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,6 @@ class Teacher < User
   belongs_to :department
   has_many :web_spaces
   mount_uploader :avatar, AvatarUploader
-  
+
   attr_accessible :name, :surname, :birthday, :office, :department_id, :research_area, :avatar
 end
