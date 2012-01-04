adef4da92a384ce236c14aca5acc13dd0724f6f1
manfred <manfred@imesmes.com>
2012-01-04 17:33:09 +0100

    Validations for teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,4 +4,7 @@ class Teacher < User
   mount_uploader :avatar, AvatarUploader
 
   attr_accessible :name, :surname, :birthday, :office, :department_id, :research_area, :avatar
+
+  validates :name,    :presence => true
+  validates :surname, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  
+
   ["Admin","Teacher"].each do |role|
     define_method "#{role.downcase}?" do
       self.type == role
