18bad6d5857f96a16dde426427e6bca89baa8de0
manfred <manfred@imesmes.com>
2012-01-18 10:42:32 +0100

    Added simple-navigation for selected header item menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,4 @@
 $(function() {
   // Hide flash messages
   setTimeout(function(){ $('.flash').hide("slow"); }, 2000);
-
-  // Add class selected for list menu
-  $('#menu li').click(function() {
-    $(this).siblings('li').removeClass('selected');
-    $(this).addClass('selected');
-  });
 });
