3f935c2b868b2d141660be5d68aa4af4d777a6ac
manfred <manfred@imesmes.com>
2012-02-16 17:47:16 +0100

    Added seeds and fixed home view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,27 @@
-Department.create(:name => 'Departament1', :description => 'Departament numero 1')
-Department.create(:name => 'Departament2', :description => 'Departament numero 2')
-Department.create(:name => 'Departament3', :description => 'Departament numero 3')
+# Departments seeds
+5.times {|n| Department.create(:name => 'Departament' + n.to_s, :description => 'Departament numero ' + n.to_s) }
 
-User.create(:name => 'teacher', :surname => 'surname', :email => 'teacher@example.com', :password => 'example', :birthday => '26-12-1986', :avatar => Rails.root+"/exemples/1.jpg", :department_id => 1, :research_area => 'Web development', :type => 'Teacher')
+# Teachers seeds
+6.times {|n| Teacher.create(
+    :email => 'teacher' + (n+1).to_s + '@example.com',
+    :password => 'example',
+    :password_confirmation => 'example',
+    :name => 'Teacher' + (n+1).to_s,
+    :surname => 'Surname' + (n+1).to_s,
+    :department_id =>  (n+1),
+    :birthday => '1986-12-26'.to_date,
+    :research_area => 'Web developers',
+    :office => 'Q3/1011',
+    :avatar => File.new(Rails.root.to_s+"/examples/" + (n+1).to_s + ".jpg")) }
+
+6.times {|n| Teacher.create(
+    :email => 'teacher' + (n+6).to_s + '@example.com',
+    :password => 'example',
+    :password_confirmation => 'example',
+    :name => 'Teacher' + (n+6).to_s,
+    :surname => 'Surname' + (n+6).to_s,
+    :department_id =>  (n+1),
+    :birthday => '1986-12-26'.to_date,
+    :research_area => 'Web design',
+    :office => 'Q3/1011',
+    :avatar => File.new(Rails.root.to_s+"/examples/" + (n+1).to_s + ".jpg")) }
