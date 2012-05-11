512f4ad315c1246ea6201a90f7618d3028659500
manfred <manfred@imesmes.com>
2012-05-11 13:16:47 +0200

    Adding translations for es/en/ca

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
+  before_filter :set_locale
   protect_from_forgery
-  
-  protected
-  
+
+private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }
+  end
+
+protected
+
   def load_departments
     @departments = Department.all
   end
