1a87978dcbf4e0aafbba4f86c013ef7d7e80f01e
manfred <manfred@imesmes.com>
2012-05-14 17:18:40 +0200

    Menu bar issues fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,14 +7,5 @@
 //= require_directory .
 
 $(function() {
-  setTimeout(function(){ $('.flash').hide("slow"); }, 2000);
-  setTimeout(function(){ $('.notice').hide("slow"); }, 2000);
-  setTimeout(function(){ $('.error').hide("slow"); }, 2000);
-
-  $(".nav li").click(function () {
-    $(this).parent().siblings().removeClass('active');
-    $(this).addClass('active');
-  });
-
   $('.dropdown-toogle').dropdown();
 });
