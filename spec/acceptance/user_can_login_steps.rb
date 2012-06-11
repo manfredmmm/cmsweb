1f0ccc9eb4a26c03d6663554e1df29942725e420
manfred <manfred@imesmes.com>
2012-06-11 17:03:13 +0200

    Style for home

---
+++ spec/acceptance/user_can_login_steps.rb
@@ -0,0 +1,13 @@
+steps_for :user_can_login do
+  step "I am not logged in" do
+  end
+
+  step "I am log in" do
+  end
+
+  step "I should I am logged in" do
+  end
+
+  step "I should be on home page" do
+  end
+end
